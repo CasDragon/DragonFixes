a1383a949fa3b78c7677d7af961d740001b7ec15
CasDragon <redyugi112@gmail.com>
2025-04-02 12:52:31 -0400

    Add fix for Shaman's elemental capstones not working correctly

---
+++ DragonFixes/Fixes/ShamanSpirits.cs
@@ -0,0 +1,77 @@
+﻿using BlueprintCore.Blueprints.Configurators.UnitLogic.ActivatableAbilities;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
+using BlueprintCore.Blueprints.References;
+using DragonFixes.Util;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Designers.Mechanics.Facts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes
+{
+    internal class ShamanSpirits
+    {
+        [DragonFix]
+        public static void PatchFireSpirit()
+        {
+            Main.log.Log("Patching Shaman Flame spirit capstone");
+            BuffConfigurator.For(BuffRefs.ShamanFlameSpiritManifestationBolsterBuff)
+                .EditComponent<AutoMetamagic>(c => c.Descriptor = SpellDescriptor.Fire)
+                .Configure();
+            BuffConfigurator.For(BuffRefs.ShamanFlameSpiritManifestationReachBuff)
+                .EditComponent<AutoMetamagic>(c => c.Descriptor = SpellDescriptor.Fire)
+                .Configure();
+            BuffConfigurator.For(BuffRefs.ShamanFlameSpiritManifestationExtendBuff)
+                .EditComponent<AutoMetamagic>(c => c.Descriptor = SpellDescriptor.Fire)
+                .Configure();
+            ActivatableAbilityConfigurator.For(ActivatableAbilityRefs.ShamanFlameSpiritManifestationExtend)
+                .SetBuff(BuffRefs.ShamanFlameSpiritManifestationExtendBuff.Reference.Get())
+                .Configure();
+        }
+        [DragonFix]
+        public static void PatchWindSpirit()
+        {
+            Main.log.Log("Patching Shaman Wind spirit capstone");
+            ActivatableAbilityConfigurator.For(ActivatableAbilityRefs.ShamanWindSpiritManifestationExtend)
+                .SetBuff(BuffRefs.ShamanWindSpiritManifestationExtendBuff.Reference.Get())
+                .Configure();
+        }
+        [DragonFix]
+        public static void PatchFrostSpirit()
+        {
+            Main.log.Log("Patching Shaman Frost spirit capstone");
+            ActivatableAbilityConfigurator.For(ActivatableAbilityRefs.ShamanFrostSpiritManifestationExtend)
+                .SetBuff(BuffRefs.ShamanFrostSpiritManifestationExtendBuff.Reference.Get())
+                .Configure();
+        }
+        [DragonFix]
+        public static void PatchWavesSpirit()
+        {
+            Main.log.Log("Patching Shaman Waves spirit capstone");
+            ActivatableAbilityConfigurator.For(ActivatableAbilityRefs.ShamanWavesSpiritManifestationExtend)
+                .SetBuff(BuffRefs.ShamanWavesSpiritManifestationExtendBuff.Reference.Get())
+                .Configure();
+        }
+        [DragonFix]
+        public static void PatchStonesSpirit()
+        {
+            Main.log.Log("Patching Shaman Stones spirit capstone");
+            BuffConfigurator.For(BuffRefs.ShamanStonesSpiritManifestationBolsterBuff)
+                .EditComponent<AutoMetamagic>(c => c.Descriptor = SpellDescriptor.Acid)
+                .Configure();
+            BuffConfigurator.For(BuffRefs.ShamanStonesSpiritManifestationReachBuff)
+                .EditComponent<AutoMetamagic>(c => c.Descriptor = SpellDescriptor.Acid)
+                .Configure();
+            BuffConfigurator.For(BuffRefs.ShamanStonesSpiritManifestationExtendBuff)
+                .EditComponent<AutoMetamagic>(c => c.Descriptor = SpellDescriptor.Acid)
+                .Configure();
+            ActivatableAbilityConfigurator.For(ActivatableAbilityRefs.ShamanStonesSpiritManifestationExtend)
+                .SetBuff(BuffRefs.ShamanStonesSpiritManifestationExtendBuff.Reference.Get())
+                .Configure();
+        }
+    }
+}
