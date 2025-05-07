7991919c9b1bcc832de8d4cf2b636d4d4a86f05f
CasDragon <redyugi112@gmail.com>
2025-05-07 16:37:41 -0400

    OtherNat Attacks nonsense

---
+++ DragonFixes/Fixes/OtherNaturalAttackNonsense.cs
@@ -0,0 +1,37 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using DragonFixes.Util;
+using Kingmaker.Blueprints.Items.Weapons;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.Enums;
+using Kingmaker.UnitLogic.Mechanics.Components;
+
+namespace DragonFixes.Fixes
+{
+    internal class OtherNaturalAttackNonsense
+    {
+        [DragonFix]
+        public static void PatchThisNonsense()
+        {
+            Main.log.Log("Patching 'OtherNaturalAttack' nonsense for Clutch of Corruption");
+            FeatureConfigurator.For(FeatureRefs.ClutchOfCorruptionFeature)
+                .EditComponent<AddInitiatorAttackWithWeaponTrigger>(c => stuff(c))
+                .Configure();
+            Main.log.Log("Patching 'OtherNaturalAttack' nonsense for Cobra Pads");
+            FeatureConfigurator.For(FeatureRefs.CobraPadsFeature)
+                .EditComponent<AddInitiatorAttackWithWeaponTrigger>(c => stuff(c))
+                .Configure();
+            Main.log.Log("Patching 'OtherNaturalAttack' nonsense for AncientWoodFeature");
+            FeatureConfigurator.For(FeatureRefs.AncientWoodFeature)
+                .RemoveComponents(c => c is ACBonusAgainstWeaponCategory)
+                .AddACBonusAgainstWeaponGroup(armorClassBonus: 3, descriptor: ModifierDescriptor.Competence, fighterGroup: WeaponFighterGroup.Natural)
+                .Configure();
+        }
+        public static void stuff(AddInitiatorAttackWithWeaponTrigger component)
+        {
+            component.CheckWeaponCategory = false;
+            component.CheckWeaponGroup = true;
+            component.Group = WeaponFighterGroup.Natural;
+        }
+    }
+}
