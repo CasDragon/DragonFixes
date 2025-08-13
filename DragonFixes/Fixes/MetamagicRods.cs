c32db82c21c596023018f8719a33e9a93c4e9c53
CasDragon <krb16us@yahoo.com>
2025-08-13 08:43:55 -0400

    Fix Piercing metamagic rods

---
+++ DragonFixes/Fixes/MetamagicRods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
+using BlueprintCore.Blueprints.References;
+using DragonFixes.Util;
+using Kingmaker.Blueprints.Classes.Spells;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.UnitLogic.Buffs.Blueprints;
+
+namespace DragonFixes.Fixes
+{
+    internal class MetamagicRods
+    {
+        [DragonFix]
+        public static void PatchPiercingRods()
+        {
+            Main.log.Log("Patching Piercing metamagic rods to actually be piercing instead of intensified.");
+            BlueprintBuff normal = BuffRefs.MetamagicRodNormalPiercingBuff.Reference.Get();
+            LibraryStuff.RemoveComponent<MetamagicRodMechanics>(normal);
+            BuffConfigurator.For(normal)
+                .AddMetamagicRodMechanics(metamagic: Kingmaker.UnitLogic.Abilities.Metamagic.Piercing,
+                    maxSpellLevel: 6, rodAbility: ActivatableAbilityRefs.MetamagicRodNormalPiercingToggleAbility.Reference.Get())
+                .Configure();
+            BlueprintBuff lesser = BuffRefs.MetamagicRodLesserPiercingBuff.Reference.Get();
+            LibraryStuff.RemoveComponent<MetamagicRodMechanics>(lesser);
+            BuffConfigurator.For(lesser)
+                .AddMetamagicRodMechanics(metamagic: Kingmaker.UnitLogic.Abilities.Metamagic.Piercing,
+                    maxSpellLevel: 3, rodAbility: ActivatableAbilityRefs.MetamagicRodLesserPiercingToggleAbility.Reference.Get())
+                .Configure();
+            BlueprintBuff greater = BuffRefs.MetamagicRodGreaterPiercingBuff.Reference.Get();
+            LibraryStuff.RemoveComponent<MetamagicRodMechanics>(greater);
+            BuffConfigurator.For(greater)
+                .AddMetamagicRodMechanics(metamagic: Kingmaker.UnitLogic.Abilities.Metamagic.Piercing,
+                    maxSpellLevel: 9, rodAbility: ActivatableAbilityRefs.MetamagicRodGreaterPiercingToggleAbility.Reference.Get())
+                .Configure();
+        }
+    }
+}
