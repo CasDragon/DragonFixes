b9dd03c9b9438e05653a360ed55be1c1733379fa
CasDragon <krb16us@yahoo.com>
2025-10-07 14:34:18 -0400

    Scribe Scrolls patch, ADDB

---
+++ DragonFixes/Fixes/ScribeScrollsPatch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Facts;
+using Kingmaker.Craft;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.UnitLogic;
+
+namespace DragonFixes.Fixes
+{
+    [HarmonyPatch]
+    internal class ScribeScrollsPatch
+    {
+        [HarmonyPatch(typeof(CraftRoot), nameof(CraftRoot.CheckCraftAvail)), HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> A(IEnumerable<CodeInstruction> instructions)
+        {
+            var method = AccessTools.Method(typeof(UnitHelper), nameof(UnitHelper.HasFact), [typeof(UnitEntityData), typeof(BlueprintFact)]);
+            var field = AccessTools.Field(typeof(CraftRequirements), nameof(CraftRequirements.RequiredFeature));
+            bool wasField = false;
+            foreach (var inst in instructions)
+            {
+                if (inst.LoadsField(field))
+                {
+                    wasField = true;
+                    yield return inst;
+                    continue;
+                }
+                else if (inst.opcode == OpCodes.Call && wasField)
+                {
+                    yield return inst;
+                    yield return new(OpCodes.Dup);
+                }
+                else if (inst.Calls(method))
+                {
+                    yield return CodeInstruction.Call((UnitEntityData unit, BlueprintFeatureReference reference, BlueprintFact fact) => ButWhatAboutEmpty(unit, reference, fact));
+                }
+                else
+                {
+                    yield return inst;
+                }
+                wasField = false;
+            }
+        }
+        private static bool ButWhatAboutEmpty(UnitEntityData unit, BlueprintFeatureReference reference, BlueprintFact fact)
+        {
+            return reference.IsEmpty() || unit.HasFact(fact);
+        }
+    }
+}
