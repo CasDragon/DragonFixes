94f9b30cc2c2f6ca63b8479481b0feab84660f21
CasDragon <krb16us@yahoo.com>
2025-11-03 08:42:06 -0500

    Fix ImmunityToAbilityScoreDamage

---
+++ DragonFixes/Fixes/AddImmunityToAbilityScoreDamageFix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic.FactLogic;
+
+namespace DragonFixes.Fixes
+{
+    [HarmonyPatch]
+    internal class AddImmunityToAbilityScoreDamageFix
+    {
+        [HarmonyPatch(typeof(AddImmunityToAbilityScoreDamage), nameof(AddImmunityToAbilityScoreDamage.OnEventAboutToTrigger))]
+        [HarmonyPrefix]
+        public static bool FixNonsense(RuleDealStatDamage evt)
+        {
+            return !evt.Immune;
+        }
+    }
+}
