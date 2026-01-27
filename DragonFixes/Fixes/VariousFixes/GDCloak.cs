ce2909b2c1692e5ba8a745b103fc11cace3a4c3b
CasDragon <krb16us@yahoo.com>
2026-01-27 11:45:35 -0500

    GD cloak

---
+++ DragonFixes/Fixes/VariousFixes/GDCloak.cs
@@ -0,0 +1,22 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes.VariousFixes
+{
+    internal class GDCloak
+    {
+        [DragonConfigure]
+        public static void PatchGDCloak()
+        {
+            Main.log.Log("Stop GD from stealing your cloak!");
+            DragonHelpers.RemoveComponent<AddFactContextActions>(FeatureRefs.DragonLevel1Immunities.Reference.Get());
+        }
+    }
+}
