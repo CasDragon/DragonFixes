6909b1205bd474407e301de94234e41ed2004631
CasDragon <krb16us@yahoo.com>
2026-01-27 16:17:34 -0500

    Seasoned Assassin not hide

---
+++ DragonFixes/Fixes/Items/SmolItemTweaks.cs
@@ -0,0 +1,23 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes.Items
+{
+    internal class SmolItemTweaks
+    {
+        [DragonConfigure]
+        public static void PatchSeasonedAssassin()
+        {
+            Main.log.Log("Patchin SeasonedAssassinFeature to not be hidden in the UI, so when you encounter an enemy with it, you can see it.");
+            FeatureConfigurator.For(FeatureRefs.SeasonedAssassinsFeature)
+                .SetHideInUI(false)
+                .Configure();
+        }
+    }
+}
