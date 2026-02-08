1c0e1e0bca80205efc45b627e26aabd10bbc6654
CasDragon <krb16us@yahoo.com>
2026-02-08 13:55:11 -0500

    Call To Violence item - add skald

---
+++ DragonFixes/Fixes/Items/CallToViolence.cs
@@ -0,0 +1,26 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes.Items
+{
+    internal class CallToViolence
+    {
+        [DragonConfigure]
+        public static void Patch()
+        {
+            Main.log.Log("Patching  to include Skald rage.");
+            BuffConfigurator.For(BuffRefs.CallToViolenceBuff)
+                .AddBuffExtraEffects(
+                    checkedBuff: BuffRefs.InspiredRageEffectBuffBeforeMasterSkald.Reference.Get(),
+                    extraEffectBuff: BuffRefs.CallToViolenceEffectBuff.Reference.Get()
+                    )
+                .Configure();
+        }
+    }
+}
