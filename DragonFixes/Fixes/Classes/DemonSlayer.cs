b33b13e30af7fe05b6decd074d1ae50753df9601
CasDragon <krb16us@yahoo.com>
2026-02-11 08:13:32 -0500

    DemonSlayer -> OrderOfTheNail FE spellcasting fix

---
+++ DragonFixes/Fixes/Classes/DemonSlayer.cs
@@ -0,0 +1,27 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes.Classes
+{
+    internal class DemonSlayer
+    {
+        [DragonConfigure]
+        public static void PatchFavoredEnemySpellcasting()
+        {
+            Main.log.Log("Patching FavoredEnemySpellcasting to include OrderOfTheNail.");
+            FeatureConfigurator.For(FeatureRefs.FavoredEnemySpellcasting)
+                .EditComponent<PrerequisiteFeaturesFromList>(c => 
+                    c.m_Features = [.. c.m_Features, 
+                        FeatureSelectionRefs.OrderOfTheNailFavoriteEnemySelection.Reference.Get().ToReference<BlueprintFeatureReference>()])
+                .Configure();
+        }
+    }
+}
