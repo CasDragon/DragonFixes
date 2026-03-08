194302058cb0662a455395394d84c02f8afc08d5
CasDragon <krb16us@yahoo.com>
2026-03-08 09:55:02 -0400

    fix herbal ring enchant

---
+++ DragonFixes/Fixes/Items/HerbalRing.cs
@@ -0,0 +1,23 @@
+﻿using BlueprintCore.Blueprints.Configurators.Items.Equipment;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes.Items
+{
+    internal class HerbalRing
+    {
+        [DragonConfigure]
+        public static void RestoreEnchant()
+        {
+            Main.log.Log("Patching HerbalRingItem to have the correct enchant.");
+            ItemEquipmentRingConfigurator.For(ItemEquipmentRingRefs.HerbalRingItem)
+                .SetEnchantments(EquipmentEnchantmentRefs.HerbalRingEnchantment.Reference.Get())
+                .Configure();
+        }
+    }
+}
