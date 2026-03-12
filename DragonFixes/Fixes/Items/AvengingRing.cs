08559bfd2de78ff20a875e029308f42ec6d642b7
CasDragon <krb16us@yahoo.com>
2026-03-12 07:28:16 -0400

    Fix Avenging Ring to work on all AoOs

---
+++ DragonFixes/Fixes/Items/AvengingRing.cs
@@ -0,0 +1,24 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using Kingmaker.UnitLogic.Mechanics.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes.Items
+{
+    internal class AvengingRing
+    {
+        [DragonConfigure]
+        public static void PatchRing()
+        {
+            Main.log.Log("Patching AvengingRingFeature to work on any AoO instead of AoO hits.");
+            FeatureConfigurator.For(FeatureRefs.AvengingRingFeature)
+                .EditComponent<AddTargetAttackWithWeaponTrigger>(c => c.OnlyHit = false)
+                .Configure();
+        }
+    }
+}
