daf1f4f7c6df06f255ecc9dd2fb88f1fa207d11d
CasDragon <krb16us@yahoo.com>
2026-05-21 14:24:34 -0400

    Reenable Rahadoum Faithless background

---
+++ DragonFixes/Fixes/Backgrounds/RahadoumFaithless.cs
@@ -0,0 +1,25 @@
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes.Selection;
+using BlueprintCore.Blueprints.References;
+using DragonLibrary.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFixes.Fixes.Backgrounds
+{
+    internal class RahadoumFaithless
+    {
+        [DragonConfigure]
+        public static void AddBackgroundRahadoumFaithless()
+        {
+            Main.log.Log("Adding the RahadoumFaithless blueprint to background selection.");
+            FeatureSelectionConfigurator.For(FeatureSelectionRefs.BackgroundsRegionalSelection)
+                .AddToAllFeatures([FeatureRefs.BackgroundRahadoumFaithless.ToString()])
+                .Configure();
+        }
+
+    }
+}
