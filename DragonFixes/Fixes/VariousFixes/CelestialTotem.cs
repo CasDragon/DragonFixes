75984535305691e90dc7e50e23d686b355b2afae
CasDragon <krb16us@yahoo.com>
2026-07-26 09:25:48 -0400

    z

--- DragonFixes/Fixes/VariousFixes/CelestialTotem.cs
+++ DragonFixes/Fixes/VariousFixes/CelestialTotem.cs
@@ -1,5 +1,6 @@
 using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
 using BlueprintCore.Blueprints.References;
+using DragonFixes.Util;
 using DragonLibrary.Utils;
 using Kingmaker.UnitLogic.FactLogic;
 
@@ -10,9 +11,12 @@ public class CelestialTotem
     [DragonConfigure]
     public static void PatchCelestialTotemLesser()
     {
-        Main.log.Log("Patching Celestial Totem Lesser to not heal on 0 hp heals.");
-        BuffConfigurator.For(BuffRefs.CelestialTotemLesserBuff)
-            .EditComponent<AddHealTrigger>(c => c.AllowZeroHealDamage = false)
-            .Configure();
+        if (Settings.GetSetting<bool>("lessercelestialtotem"))
+        {
+            Main.log.Log("Patching Celestial Totem Lesser to not heal on 0 hp heals.");
+            BuffConfigurator.For(BuffRefs.CelestialTotemLesserBuff)
+                .EditComponent<AddHealTrigger>(c => c.AllowZeroHealDamage = false)
+                .Configure();
+        }
     }
 }
