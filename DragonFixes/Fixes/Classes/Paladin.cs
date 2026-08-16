ca0824b7790cf246a1aa6c9d2b003e849de9a557
CasDragon <krb16us@yahoo.com>
2026-08-16 08:45:05 -0400

    z

--- DragonFixes/Fixes/Classes/Paladin.cs
+++ DragonFixes/Fixes/Classes/Paladin.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using BlueprintCore.Blueprints.References;
+using DragonLibrary.NewComponents;
 using DragonLibrary.Utils;
 using Kingmaker.Blueprints;
 using Kingmaker.Designers.EventConditionActionSystem.Actions;
@@ -14,17 +15,21 @@ public class Paladin
     [DragonConfigure]
     public static void noFunAllowed()
     {
-        Main.log.Log("Fixing Tortured Crusader's Last Man Standing feature to not scale off summoned spiders.");
+        Main.log.Log("Fixing Tortured Crusader's Alone in the Dark - Last Man Standing feature to not scale off summoned spiders.");
         var x = new ContextConditionHasBuff()
         {
             m_Buff = BuffRefs.ClemencyOfShadowsExclusionBuff.Reference.Get().ToReference<BlueprintBuffReference>(),
             Not = true
         };
+        var y = new ConditionIsFaction()
+        {
+            Faction = FactionRefs.Summoned.Reference.Get()
+        };
         var runAction = AbilityAreaEffectRefs.LastManAreaEffect.Reference.Get()
             .GetComponent<AbilityAreaEffectRunAction>();
         var condition1 = runAction!.UnitEnter.Actions.First(c => c is Conditional) as Conditional;
-        condition1!.ConditionsChecker.Conditions = [.. condition1.ConditionsChecker.Conditions, x];
+        condition1!.ConditionsChecker.Conditions = [.. condition1.ConditionsChecker.Conditions, x, y];
         var condition2 = runAction!.Round.Actions.First(c => c is Conditional) as Conditional;
-        condition2!.ConditionsChecker.Conditions = [.. condition2.ConditionsChecker.Conditions, x];
+        condition2!.ConditionsChecker.Conditions = [.. condition2.ConditionsChecker.Conditions, x, y];
     }
 }
