1a9f6ec0c87f932629d9f7e938564897bcec0c44
Delfayne <delfayne@users.noreply.github.com>
2026-08-17 16:38:33 +0900

    Fixing kineticist damage scaling on IceBlasts and metal Deadly Earth

--- DragonFixes/Fixes/Classes/Kineticist.cs
+++ DragonFixes/Fixes/Classes/Kineticist.cs
@@ -3,8 +3,10 @@ using BlueprintCore.Blueprints.References;
 using BlueprintCore.Utils;
 using DragonLibrary.Utils;
 using Kingmaker.Blueprints;
+using Kingmaker.Enums;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
 using Kingmaker.UnitLogic.Class.Kineticist;
+using Kingmaker.UnitLogic.Mechanics.Components;
 
 namespace DragonFixes.Fixes.Classes;
 
@@ -37,5 +39,55 @@ public class Kineticist
         {
             component.BlastBurnCost = 0;
             component.InfusionBurnCost = 2;
+        }
+
+        /// <summary>
+        /// A composite blast should deal double a simple blast's dice with full Constitution -
+        /// i.e. step2/AsIs (only Blue Flame is an energy composite, which halves Con).
+        ///
+        /// Metal's two Deadly Earth areas stack step1/Div2 on top of
+        /// Half:true, halving twice.
+        ///
+        /// Ice Blast, blade and Fragmentation sit at step1/Div2, dealing simple-blast damage
+        /// at composite burn cost. Ice's Spindle and Wall are already correct; leave them.
+        /// </summary>
+        [DragonConfigure]
+        public static void FixCompositeBlastDamageScaling()
+        {
+            Main.log.Log("Fixing Ice Blast and Metal Deadly Earth to deal composite blast damage instead of simple blast damage.");
+            Blueprint<BlueprintReference<BlueprintAbility>>[] iceBlastAbilities = [
+                AbilityRefs.IceBlastAbility,
+                AbilityRefs.IceBlastBladeDamage,
+                AbilityRefs.FragmentationIceBlastAbility
+            ];
+            foreach (var iceBlastAbility in iceBlastAbilities)
+            {
+                AbilityConfigurator.For(iceBlastAbility)
+                    .EditComponents<ContextRankConfig>(RestoreCompositeScaling, c => true)
+                    .Configure();
+            }
+            Blueprint<BlueprintReference<BlueprintAbilityAreaEffect>>[] deadlyEarthAreas = [
+                AbilityAreaEffectRefs.DeadlyEarthMetalBlastArea,
+                AbilityAreaEffectRefs.DeadlyEarthMetalBlastAreaRare
+            ];
+            foreach (var deadlyEarthArea in deadlyEarthAreas)
+            {
+                AbilityAreaEffectConfigurator.For(deadlyEarthArea)
+                    .EditComponents<ContextRankConfig>(RestoreCompositeScaling, c => true)
+                    .Configure();
+            }
+        }
+
+        private static void RestoreCompositeScaling(ContextRankConfig config)
+        {
+            if (config.m_Type == AbilityRankType.DamageDice)
+            {
+                config.m_Progression = ContextRankProgression.MultiplyByModifier;
+                config.m_StepLevel = 2;
+            }
+            else if (config.m_Type == AbilityRankType.DamageBonus)
+            {
+                config.m_Progression = ContextRankProgression.AsIs;
+            }
         }
 }
